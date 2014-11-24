5babb84bb1e751ed0f3a6387212a3d2306c1f2f4
jimenbian <jimenbian@gmail.com>
2014-11-24 22:08:06 +0800

    add Bisearch

---
+++ src/Algorithm_analysis/Bisearch.java
@@ -0,0 +1,35 @@
+package Algorithm_analysis;
+
+public class Bisearch {
+
+	static int[] array={-3,-2,0,4,5,7,12,64}; 
+	public static void main(String args[]){
+	   int left=0;
+	   int right=array.length;
+	   int center=0;
+	   int k=7;
+	  
+	   while(left<=right){
+		  center=(right+left)/2;
+		   if ((array[center]-k)==0){
+			  System.out.print(center);
+			  break;
+		   }
+		   else{
+            if((array[center]-k)>0){
+                 right=center;
+            }
+                 else{
+                	 left=center;
+                 }
+                	 }
+                 }
+	   }
+	
+	
+		
+	}
+	
+	
+	
+
